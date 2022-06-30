ee5afc70e641ba7e2189aaba7d38269fe6169107
Max Sharan <qshark55@gmail.com>
2022-07-01 01:27:53 +0300

    Homework 01 find max and min from 2 numbers

---
+++ Homework01_2-numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int a = new Random().Next(-1000,1000);
+int b = new Random().Next(-1000,1000);
+
+int max = a;
+int min = b;
+
+if(a < b) 
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("Среди чисел: " + a + " и " + b);
+Console.WriteLine("Максимальное число: " + max);
+Console.WriteLine("Минимальное число: " + min);
