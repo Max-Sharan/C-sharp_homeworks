8828691055d2340d8ff19e330e1eff65bdd6ad64
Max Sharan <qshark55@gmail.com>
2022-07-01 02:07:20 +0300

    Homework 003 print chet or nechet number

---
+++ Homework03_chet-number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+int x = new Random().Next(-1000,1000);
+
+if(x % 2 == 0) 
+{
+   Console.WriteLine("Число: " + x + ", - чётное");
+}
+else 
+{
+    Console.WriteLine("Число: " + x + ", - нечётное");
+}
