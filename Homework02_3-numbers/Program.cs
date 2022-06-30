69d0851b99617e632f278e862bbeeb22321e96a6
Max Sharan <qshark55@gmail.com>
2022-07-01 01:51:48 +0300

    Homework 02 find max from 3 numbers

---
+++ Homework02_3-numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int x = new Random().Next(-1000,1000);
+int y = new Random().Next(-1000,1000);
+int z = new Random().Next(-1000,1000);
+
+int maxFrom(int x, int y, int z)
+{
+    int max = x;
+    if(y > x) max = y;
+    if(z > y) max = z;
+    Console.WriteLine("Среди чисел: " + x + " " + y + " " + z);
+    Console.WriteLine("Максимальное число: " + max);
+    return max;
+}
+
+maxFrom(x, y, z);
