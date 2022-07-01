70d187ed69e7a4788d8399fd0ada6c4894569e71
Max Sharan <qshark55@gmail.com>
2022-07-02 02:58:31 +0300

    Homework 2 second num from user number

---
+++ Homework-2-second_num-from_number/Program.cs
@@ -0,0 +1,17 @@
+﻿// -------------- Вывод второй цифры терхзначного числа ------------- //
+Console.Clear();
+
+Console.Write("Введите целое трехзначное число (от 100 до 999): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(number > 999 || number < 100)
+{
+    Console.WriteLine();
+    Console.WriteLine("Надо было ввести число (от 100 до 999)");
+    Console.Write("Повторите ввод числа (от 100 до 999): ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+int secondNumber = number % 100 / 10;
+Console.WriteLine("Вторая цифра числа " + number + " - это " + secondNumber);
+Console.WriteLine();
