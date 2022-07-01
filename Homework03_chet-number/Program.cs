44c67bc473b30c71f046e0c1969b8f1bfa1a3def
Max Sharan <qshark55@gmail.com>
2022-07-02 01:04:39 +0300

    Homework 03 chet number by user or by computer - user choice

--- Homework03_chet-number/Program.cs
+++ Homework03_chet-number/Program.cs
@@ -1,12 +1,26 @@
 ﻿Console.Clear();
 
-int x = new Random().Next(-1000,1000);
+Console.WriteLine("Вы доверяете компьютеру выбрать число?");
+Console.Write("Введите да или нет: ");
+string userAnswer = Console.ReadLine();
 
-if(x % 2 == 0) 
+int x = 0;
+
+if (userAnswer.ToLower() == "да")
 {
-   Console.WriteLine("Число: " + x + ", - чётное");
+    x = new Random().Next(-100000,100000);
 }
 else 
 {
-    Console.WriteLine("Число: " + x + ", - нечётное");
+    Console.Write("Введите своё число: ");
+    x = Convert.ToInt32(Console.ReadLine());
+}
+
+if(x % 2 == 0) 
+{
+   Console.WriteLine("Число: " + x + " - чётное");
+}
+else 
+{
+    Console.WriteLine("Число: " + x + " - нечётное");
 }
