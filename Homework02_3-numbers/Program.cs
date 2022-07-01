7401443c0bf3f9eadc04f9052abe890411302ab0
Max Sharan <qshark55@gmail.com>
2022-07-02 00:59:45 +0300

    Homework 02 3 number by user or by computer - user choice

--- Homework02_3-numbers/Program.cs
+++ Homework02_3-numbers/Program.cs
@@ -1,15 +1,36 @@
 ﻿Console.Clear();
 
-int x = new Random().Next(-1000,1000);
-int y = new Random().Next(-1000,1000);
-int z = new Random().Next(-1000,1000);
+Console.WriteLine("Вы доверяете компьютеру выбрать числа?");
+Console.Write("Введите да или нет: ");
+string userAnswer = Console.ReadLine();
+
+int x = 0;
+int y = 0;
+int z = 0;
+
+if(userAnswer.ToLower() == "да")
+{
+    x = new Random().Next(-1000,1000);
+    y = new Random().Next(-1000,1000);
+    z = new Random().Next(-1000,1000);
+}
+else 
+{
+    Console.Write("Введите первое число: ");
+    x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    y = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите третье число: ");
+    z = Convert.ToInt32(Console.ReadLine());
+}
 
 int maxFrom(int x, int y, int z)
 {
     int max = x;
     if(y > x) max = y;
     if(z > y) max = z;
-    Console.WriteLine("Среди чисел: " + x + " " + y + " " + z);
+    Console.WriteLine();
+    Console.WriteLine("Среди чисел: " + x + ", " + y + " и " + z);
     Console.WriteLine("Максимальное число: " + max);
     return max;
 }
