6abdfe51047c9c3213e6c353dc8bee30af2c30e1
Max Sharan <qshark55@gmail.com>
2022-07-02 04:45:48 +0300

    Homework 2: day of the week - is a weekend?

---
+++ Homework-2-day_of_week/Program.cs
@@ -0,0 +1,24 @@
+﻿// -------------- Вывод выходного дня недели по номеру ------------- //
+Console.Clear();
+
+Console.Write("Введите номер дня недели (от 1 до 7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+int count = 7;
+
+while (day > count)
+{
+    Console.WriteLine("Прости, друг! В неделе только 7 дней!");
+    Console.WriteLine();
+    Console.Write("Введите номер дня недели (от 1 до 7): ");
+    day = Convert.ToInt32(Console.ReadLine());
+}
+count -=2;
+if (day <= count)
+{
+    Console.WriteLine("Ещё не выходной день,- трудимся дальше!");
+    count +=2;
+}
+else
+{
+    Console.WriteLine("УРА! Выходной день,- отдыхаем!");
+}
