6e7a2c55d1bee407f05686104dfb756665d113ab
Max Sharan <qshark55@gmail.com>
2022-07-02 04:25:25 +0300

    Homework 2 third num of any number if it's were

---
+++ Homework-2-third_num-of_number/Program.cs
@@ -0,0 +1,22 @@
+﻿// -------------- Вывод третьей цифры любого целого числа, если она есть ------------- //
+Console.Clear();
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdNum = 0;
+int count = 1;
+
+if(number < count * 100)
+{
+    Console.WriteLine();
+    Console.WriteLine("Третьей цифры у числа " + number + " нет!");
+}    
+else
+{   while(number > count * 100)
+    {
+        thirdNum = number / count % 10;
+        count *= 10;
+    }
+    Console.WriteLine();
+    Console.WriteLine("Третья цифра у числа " + number + " - это " + thirdNum);
+}   
