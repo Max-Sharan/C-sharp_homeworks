3da6e5c227fe8674426af3902e7b2ac8bf967520
Max Sharan <qshark55@gmail.com>
2022-07-03 13:05:47 +0300

    Homework 3 cube of number

---
+++ Homework-3_cube-of-N/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите пожалуйста целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num = 1;
+Console.Write("Числа от 1 до " + number + " в кубе : ");
+
+while (num <= number)
+{
+    int cube = num * num * num;
+    Console.Write("| " + cube + " |");
+    num++;
+}
