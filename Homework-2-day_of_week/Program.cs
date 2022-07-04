4867b67990015250d9235b44bd7dfee7e3308415
Max Sharan <qshark55@gmail.com>
2022-07-04 19:44:19 +0300

    Homework 2 day of the week, rewrite condions for negative numbers and 0

--- Homework-2-day_of_week/Program.cs
+++ Homework-2-day_of_week/Program.cs
@@ -5,18 +5,36 @@ Console.Write("Введите номер дня недели (от 1 до 7): ")
 int day = Convert.ToInt32(Console.ReadLine());
 int count = 7;
 
-while (day > count)
+while (day > count || day <= count - 7)
 {
-    Console.WriteLine("Прости, друг! В неделе только 7 дней!");
-    Console.WriteLine();
-    Console.Write("Введите номер дня недели (от 1 до 7): ");
-    day = Convert.ToInt32(Console.ReadLine());
+    if (day > count)
+    {
+        Console.WriteLine("Прости, друг! В неделе только 7 дней!");
+        Console.WriteLine();
+        Console.Write("Введите номер дня недели (от 1 до 7): ");
+        day = Convert.ToInt32(Console.ReadLine());
+    }
+    if (day == count - 7)
+    {
+        Console.WriteLine("Прости, друг! Номер дня недели не может быть = 0!");
+        Console.WriteLine();
+        Console.Write("Введите номер дня недели (от 1 до 7): ");
+        day = Convert.ToInt32(Console.ReadLine());
+    }
+    else 
+    {
+        Console.WriteLine("Прости, друг! Номер дня недели не может быть отрицательным числом!");
+        Console.WriteLine();
+        Console.Write("Введите номер дня недели (от 1 до 7): ");
+        day = Convert.ToInt32(Console.ReadLine());
+    }
 }
-count -=2;
+
+count -= 2;
 if (day <= count)
 {
     Console.WriteLine("Ещё не выходной день,- трудимся дальше!");
-    count +=2;
+    count += 2;
 }
 else
 {
