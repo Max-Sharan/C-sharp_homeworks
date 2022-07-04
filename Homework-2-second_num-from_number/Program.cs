b4a11600f151261f08ad92a313ad4a12201b0126
Max Sharan <qshark55@gmail.com>
2022-07-04 23:19:30 +0300

    homework 2 rewrite conditions and while cicles

--- Homework-2-second_num-from_number/Program.cs
+++ Homework-2-second_num-from_number/Program.cs
@@ -1,17 +1,39 @@
 ﻿// -------------- Вывод второй цифры терхзначного числа ------------- //
 Console.Clear();
 
-Console.Write("Введите целое трехзначное число (от 100 до 999): ");
+Console.Write("Введите целое трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-while(number > 999 || number < 100)
+while (number < -999 || number > 999)
 {
     Console.WriteLine();
-    Console.WriteLine("Надо было ввести число (от 100 до 999)");
-    Console.Write("Повторите ввод числа (от 100 до 999): ");
+    Console.WriteLine("Надо было ввести трехзначное число");
+    Console.Write("Повторите ввод числа (от 100 до 999 или от -999 до -100): ");
     number = Convert.ToInt32(Console.ReadLine());
 }
-Console.WriteLine();
-int secondNumber = number % 100 / 10;
-Console.WriteLine("Вторая цифра числа " + number + " - это " + secondNumber);
-Console.WriteLine();
+while (number < 100 && number > -100)
+{
+    Console.WriteLine();
+    Console.WriteLine("Надо было ввести трехзначное число");
+    Console.Write("Повторите ввод числа (от 100 до 999 или от -999 до -100): ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+if (number > -1000 && number < -99)
+{
+    Console.WriteLine();
+    int secondNumber = -number % 100 / 10;
+    Console.WriteLine("Вторая цифра числа " + number + " - это " + secondNumber);
+    Console.WriteLine();
+}
+else if (number > 99 && number < 1000)
+{
+    Console.WriteLine();
+    int secondNumber = number % 100 / 10;
+    Console.WriteLine("Вторая цифра числа " + number + " - это " + secondNumber);
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("Упс! Надо было ввести ТрехЗначное число! :-)");
+}
