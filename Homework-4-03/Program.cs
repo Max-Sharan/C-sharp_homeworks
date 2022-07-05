e09e5a42d4660c31ae6218725d25ad6edb7b4868
Max Sharan <qshark55@gmail.com>
2022-07-06 01:29:57 +0300

    Homework 4 03 array from N number

---
+++ Homework-4-03/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 0) n = -n;
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    Console.WriteLine("Введите элементы массива: ");
+    int index = 0;
+
+    for (index = 0; index < length; index++)
+    {
+        array[index] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("Сгенерированный массив:");
+    Console.Write("[");
+    for (index = 0; index < length - 1; index++)
+    {
+        Console.Write(array[index] + ", ");
+    }
+    Console.Write(array[index] + "]");
+}
+
+int[] mass = new int[n];
+PrintArray(mass);
