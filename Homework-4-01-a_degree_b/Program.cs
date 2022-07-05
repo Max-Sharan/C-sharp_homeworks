3a2d07e4dde3d4f3d6de7309ed449cc3a632b864
Max Sharan <qshark55@gmail.com>
2022-07-05 23:51:13 +0300

    homework 4_01 a degree b

---
+++ Homework-4-01-a_degree_b/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите положительное целое число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите положительное целое число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int aDegreeB = a;
+
+for (int count = 1; count < b; count++)
+{
+    aDegreeB *= a;
+}
+Console.WriteLine("Ура! Мы возвели число A = " + a + ", в степень B = " + b + " и получилось: " + aDegreeB);
