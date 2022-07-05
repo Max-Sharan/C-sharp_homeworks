329f93b69418844beb7757974533223526ba5bc8
Max Sharan <qshark55@gmail.com>
2022-07-06 00:32:18 +0300

    Homework 4 02 summ figures of the number

---
+++ Homework-4-02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите целое число Number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number = -number;
+
+int sum = number % 10;
+
+if (number / 10 == 0)
+{
+    Console.WriteLine("Сумма цифр в вашем числе = " + sum);
+}
+else
+{
+    while (number / 10 != 0)
+    {
+        number /= 10;
+        sum = sum + number % 10;
+    }
+    Console.WriteLine("Сумма цифр в вашем числе = " + sum);
+}
