1906363504975a1d6d0de1346eb1851d7cce231a
Max Sharan <qshark55@gmail.com>
2022-07-13 21:22:02 +0300

    Homewrok 6-01 well done by methods

---
+++ Homework-6-01/Program.cs
@@ -0,0 +1,18 @@
+﻿int InputInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void CountNumbers(int count, int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        int number = InputInt("Вводите любые целые числа: ");
+        if (number > 0)
+            count++;
+    }
+    Console.WriteLine($"Кол-во чисел больше 0 = {count}");
+}
+
+int num = InputInt("Какое кол-во чисел хотите ввести? ");
+CountNumbers(0, num);
