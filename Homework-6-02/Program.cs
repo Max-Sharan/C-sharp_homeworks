3f29b6992a84cc5d61af48e2f445b7f7d61565ea
Max Sharan <qshark55@gmail.com>
2022-07-16 00:28:36 +0300

    coordinate of cross lines well done

---
+++ Homework-6-02/Program.cs
@@ -0,0 +1,26 @@
+﻿int InputInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void CoordCrossPoint(int a, int b, int c, int d)
+{
+    if (a == c && b == d)
+        Console.WriteLine("Прямые полностью совпадают");
+    else if (b == d)
+        Console.WriteLine("Прямые параллельны друг другу и никогда не пересекутся!");
+    else
+    {
+        double y = Convert.ToDouble(a * d - c * b) / (d - b);
+        double x = (y - a) / b;
+        Console.WriteLine($"Координаты точки пересечения прямых ({x}; {y})");
+    }
+}
+
+int b1 = InputInt("Введите 1е число: ");
+int k1 = InputInt("Введите 2е число: ");
+int b2 = InputInt("Введите 3е число: ");
+int k2 = InputInt("Введите 4е число: ");
+
+CoordCrossPoint(b1, k1, b2, k2);
+
