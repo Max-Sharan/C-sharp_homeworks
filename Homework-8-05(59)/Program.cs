5bba658004b9b83038f0811b9b360bcc1215680f
Max Sharan <qshark55@gmail.com>
2022-07-25 00:20:12 +0300

    homework 8 06 spiral array well done

---
+++ Homework-8-05(59)/Program.cs
@@ -0,0 +1,63 @@
+﻿
+int InputInt(string? msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+int numRows = InputInt("Введите количество строк: ");
+int numCols = InputInt("Введите количество столбцов: ");
+
+int[,] numbers = new int[numRows, numCols];
+FillArray(numbers, 1);
+PrintArray(numbers);
+void FillArray(int[,] matrix, int k, int i = 0, int j = 0)
+{
+    matrix[i, j] = 1;
+    for (j = 0; j < matrix.GetLength(1) - k; j++)
+    {
+        matrix[i, j + 1] = matrix[i, j] + 1;
+    }
+    for (i = 0; i < matrix.GetLength(0) - k; i++)
+    {
+        matrix[i + 1, j] = matrix[i, j] + 1;
+    }
+    for (j = matrix.GetLength(1) - k; j > 0; j--)
+    {
+        matrix[i, j - 1] = matrix[i, j] + 1;
+    }
+    for (i = matrix.GetLength(0) - k; i > k; i--)
+    {
+        matrix[i - 1, j] = matrix[i, j] + 1;
+    }
+    k++;
+    for (j = 0; j < matrix.GetLength(1) - k; j++)
+    {
+        matrix[i, j + 1] = matrix[i, j] + 1;
+    }
+    for (i = k - 1; i < matrix.GetLength(0) - k; i++)
+    {
+        matrix[i + 1, j] = matrix[i, j] + 1;
+    }
+    for (j = matrix.GetLength(1) - k; j > k - 1; j--)
+    {
+        matrix[i, j - 1] = matrix[i, j] + 1;
+    }
+}
+
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < 10)
+                Console.Write(matrix[i, j] + "  ");
+            else
+                Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Homework-8-06(62)/Program.cs
@@ -0,0 +1,57 @@
+﻿void FillArray(int[,] matrix, int k = 1, int i = 0, int j = 0, int count = 1)
+{
+    while (count <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        for (j = k - 1; j < matrix.GetLength(1) - k; j++)
+        {
+            if (matrix[i, j] == 0)
+                matrix[i, j] = count++;
+        }
+        for (i = k - 1; i < matrix.GetLength(0) - k; i++)
+        {
+            if (matrix[i, j] == 0)
+                matrix[i, j] = count++;
+        }
+        for (j = matrix.GetLength(1) - k; j > k - 1; j--)
+        {
+            if (matrix[i, j] == 0)
+                matrix[i, j] = count++;
+        }
+        for (i = matrix.GetLength(0) - k; i > k; i--)
+        {
+            if (matrix[i, j] == 0)
+                matrix[i, j] = count++;
+        }
+        if (matrix[i, j] == 0)
+            matrix[i, j] = count++;
+        k++;
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < 10)
+                Console.Write(matrix[i, j] + "  ");
+            else
+                Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int InputInt(string? msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+int numRows = InputInt("Введите количество строк: ");
+int numCols = InputInt("Введите количество столбцов: ");
+
+int[,] numbers = new int[numRows, numCols];
+FillArray(numbers);
+PrintArray(numbers);
