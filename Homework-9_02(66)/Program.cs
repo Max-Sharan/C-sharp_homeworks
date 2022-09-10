1cbc2bc09f706dd7498347002fe9ac207ac1a6cc
Max Sharan <qshark55@gmail.com>
2022-09-10 20:48:49 +0300

    task 66 summ of natural numbers from M to N

---
+++ Homework-9_02(66)/Program.cs
@@ -0,0 +1,23 @@
+﻿int m = InputInt("Введите число M: ");
+int n = InputInt("Введите число N: ");
+
+int summ = 0;
+
+Console.WriteLine(Summa(m, n));
+
+int Summa(int m, int n)
+{
+    if (n == m)
+        return n;
+    else if (n < m)
+        summ = n + Summa(m, n + 1);
+    if (n > m)
+        summ = m + Summa(m + 1, n);
+    return summ;
+}
+
+int InputInt(string? msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
