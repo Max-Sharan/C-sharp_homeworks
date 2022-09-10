f0c2dbad91e9e527c77f54628c165433aad0d724
Max Sharan <qshark55@gmail.com>
2022-09-10 21:35:11 +0300

    task 68 Akkerman function

---
+++ Homework-9_03(68)/Program.cs
@@ -0,0 +1,22 @@
+﻿int m = InputInt("Введите число M: ");
+int n = InputInt("Введите число N: ");
+
+Console.WriteLine($"A({m},{n}) = {AckermannFunction(m, n)}");
+
+int AckermannFunction (int m, int n)
+{
+    if (m == 0) 
+        return n + 1;
+    else
+        if (m != 0 && n == 0) 
+            return AckermannFunction(m - 1, 1);
+        else
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+
+}
+
+int InputInt(string? msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
