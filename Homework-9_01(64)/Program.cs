1d7c40f0b77acd83f620cafd093c42dc1deb1950
Max Sharan <qshark55@gmail.com>
2022-09-10 20:48:01 +0300

    task 64 natural numbers from M to N

---
+++ Homework-9_01(64)/Program.cs
@@ -0,0 +1,19 @@
+﻿int m = InputInt("Введите число M: ");
+int n = InputInt("Введите число N: ");
+Console.WriteLine(Progression(m, n));
+
+int Progression(int m, int n)
+{
+    if (n == m)
+        return m;
+    else if (n < m)
+        Console.Write(Progression(m, n + 1) + ", ");
+    if (n > m)
+        Console.Write(Progression(m, n - 1) + ", ");
+    return n;
+}
+int InputInt(string? msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
